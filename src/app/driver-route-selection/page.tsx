749fcdeffcdfa36e61ccb9265106373c29d9d33e
rajeevbackup42112-coder <rajeev.backup4.2112@gmail.com>
2026-08-16 20:37:37 +0530

    Add driver route selection page

---
+++ src/app/driver-route-selection/page.tsx
@@ -0,0 +1,5 @@
+import DriverRouteSelectionContent from './components/DriverRouteSelectionContent';
+
+export default function DriverRouteSelectionPage() {
+  return <DriverRouteSelectionContent />;
+}
