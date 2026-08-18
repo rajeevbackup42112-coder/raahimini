00b02d3e76c6639534f01128d941b5e494232211
rajeevbackup42112-coder <rajeev.backup4.2112@gmail.com>
2026-08-18 10:28:19 +0530

    Refresh passenger status from safe invalidations

--- src/app/request-status-screen/page.tsx
+++ src/app/request-status-screen/page.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import AppLayout from '@/components/AppLayout';
 import RequestStatusRouter from './components/RequestStatusRouter';
+import RealtimeRefreshBoundary from '@/components/RealtimeRefreshBoundary';
 
 export default function RequestStatusPage() {
   return (
     <AppLayout headerTitle="Your Request" headerBack>
-      <RequestStatusRouter />
+      <RealtimeRefreshBoundary>
+        <RequestStatusRouter />
+      </RealtimeRefreshBoundary>
     </AppLayout>
   );
 }
