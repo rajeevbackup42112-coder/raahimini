f72163d6ffcbd171e9f30151f83e6558a9b9b01a
rajeevbackup42112-coder <rajeev.backup4.2112@gmail.com>
2026-08-21 10:32:08 +0530

    feat: surface unserved demand on admin home

--- src/app/admin-panel/page.tsx
+++ src/app/admin-panel/page.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import AdminPanelContent from './components/AdminPanelContent';
 import AdminQuickActions from './components/AdminQuickActions';
 import AdminEntryLink from './components/AdminEntryLink';
+import AdminDemandOverview from './components/AdminDemandOverview';
 
 export default function AdminPanelPage() {
   return (
     <div className="min-h-screen bg-background">
       <AdminEntryLink />
       <AdminQuickActions />
+      <AdminDemandOverview />
       <AdminPanelContent />
     </div>
   );
