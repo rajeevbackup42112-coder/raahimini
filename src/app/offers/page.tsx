508c054aa7d016f7dc10f1a49b3bcdd0737a31be
rajeevbackup42112-coder <rajeev.backup4.2112@gmail.com>
2026-08-30 15:01:30 +0530

    feat: add Local Offers page

---
+++ src/app/offers/page.tsx
@@ -0,0 +1,6 @@
+import AppLayout from '@/components/AppLayout';
+import LocalOffersContent from './LocalOffersContent';
+
+export default function LocalOffersPage(){
+  return <AppLayout headerTitle="Local Offers" headerBack><LocalOffersContent/></AppLayout>;
+}
