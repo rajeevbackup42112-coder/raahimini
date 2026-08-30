df0dc4e81628187ad70ba52804f4c54a57aa94c8
rajeevbackup42112-coder <rajeev.backup4.2112@gmail.com>
2026-08-30 13:31:35 +0530

    feat: surface Outstation tab on Passenger Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import AppLayout from '@/components/AppLayout';
 import LocationContent from './components/LocationContent';
 import RealtimeRefreshBoundary from '@/components/RealtimeRefreshBoundary';
+import RideModeTabs from '@/components/RideModeTabs';
 
 export default function LocationPage() {
   return (
     <AppLayout>
+      <RideModeTabs active="shared" />
       <RealtimeRefreshBoundary>
         <LocationContent />
       </RealtimeRefreshBoundary>
