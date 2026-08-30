924670a634c39ed8889c4c5f58397f569c086c4c
rajeevbackup42112-coder <rajeev.backup4.2112@gmail.com>
2026-08-30 13:30:05 +0530

    feat: add Outstation Passenger route

---
+++ src/app/outstation/page.tsx
@@ -0,0 +1,6 @@
+import AppLayout from '@/components/AppLayout';
+import OutstationContent from './OutstationContent';
+
+export default function OutstationPage(){
+  return <AppLayout><OutstationContent/></AppLayout>;
+}
