43e51b074187d783f376004b0df49352d1a1a6f5
rajeevbackup42112-coder <rajeev.backup4.2112@gmail.com>
2026-08-30 15:02:48 +0530

    feat: show sponsored Local Offer on Passenger home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import AppLayout from '@/components/AppLayout';
 import LocationContent from './components/LocationContent';
 import RealtimeRefreshBoundary from '@/components/RealtimeRefreshBoundary';
 import RideModeTabs from '@/components/RideModeTabs';
+import SponsoredLocalOffer from '@/components/SponsoredLocalOffer';
 
 export default function LocationPage() {
   return (
@@ -11,6 +12,7 @@ export default function LocationPage() {
       <RealtimeRefreshBoundary>
         <LocationContent />
       </RealtimeRefreshBoundary>
+      <SponsoredLocalOffer />
     </AppLayout>
   );
 }
